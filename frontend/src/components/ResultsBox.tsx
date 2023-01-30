227e04a11042d47124f48001b825acf8965fbf3d
cvele3 <cvetkojakov@gmail.com>
2023-01-30 09:22:15 +0100

    results change

--- frontend/src/components/ResultsBox.tsx
+++ frontend/src/components/ResultsBox.tsx
@@ -1,72 +1,64 @@
-import React from 'react'
-import { Card, CardHeader, CardBody } from 'reactstrap'
-import ShowResultOfTheGame from './ShowResultOfTheGame'
+import React, { useState } from 'react';
+import { Card, CardHeader, CardBody } from 'reactstrap';
+import ShowResultOfTheGame from './ShowResultOfTheGame';
 
 const Results = () => {
+  const [firstTeamGoals, setFirstTeamGoals] = useState([1, 1, 1, 1, 1, 1]);
+  const [secondTeamGoals, setSecondTeamGoals] = useState([1, 1, 1, 1, 1, 1]);
+
+  const handleSave = (index: number, editedFirstTeamGoals: number, editedSecondTeamGoals: number) => {
+    setFirstTeamGoals((prevFirstTeamGoals) => {
+      const updatedFirstTeamGoals = [...prevFirstTeamGoals];
+      updatedFirstTeamGoals[index] = editedFirstTeamGoals;
+      return updatedFirstTeamGoals;
+    });
+    setSecondTeamGoals((prevSecondTeamGoals) => {
+      const updatedSecondTeamGoals = [...prevSecondTeamGoals];
+      updatedSecondTeamGoals[index] = editedSecondTeamGoals;
+      return updatedSecondTeamGoals;
+    });
+  };
+
   return (
-    <Card style={{marginBottom: '200px'}}>
+    <Card style={{ marginBottom: '200px' }}>
       <CardHeader className="fixed-height-card-header">Results</CardHeader>
       <CardBody>
         {/* Results content goes here */}
         <ShowResultOfTheGame
           homeClub="Home Club"
           awayClub="Away Club"
-          firstTeamGoals={1}
-          secondTeamGoals={1}
-          onSave={(firstTeamGoals: number, secondTeamGoals: number) => console.log('${firstTeamGoals}-${secondTeamGoals}')}
+          firstTeamGoals={firstTeamGoals[0]}
+          secondTeamGoals={secondTeamGoals[0]}
+          onSave={(editedFirstTeamGoals, editedSecondTeamGoals) => handleSave(0, editedFirstTeamGoals, editedSecondTeamGoals)}
         />
         <hr />
         <ShowResultOfTheGame
           homeClub="Home Club"
           awayClub="Away Club"
-          firstTeamGoals={1}
-          secondTeamGoals={1}
-          onSave={(firstTeamGoals: number, secondTeamGoals: number) => console.log('${firstTeamGoals}-${secondTeamGoals}')}
+          firstTeamGoals={firstTeamGoals[1]}
+          secondTeamGoals={secondTeamGoals[1]}
+          onSave={(editedFirstTeamGoals, editedSecondTeamGoals) => handleSave(1, editedFirstTeamGoals, editedSecondTeamGoals)}
         />
         <hr />
         <ShowResultOfTheGame
           homeClub="Home Club"
           awayClub="Away Club"
-          firstTeamGoals={1}
-          secondTeamGoals={1}
-          onSave={(firstTeamGoals: number, secondTeamGoals: number) => console.log('${firstTeamGoals}-${secondTeamGoals}')}
+          firstTeamGoals={firstTeamGoals[2]}
+          secondTeamGoals={secondTeamGoals[2]}
+          onSave={(editedFirstTeamGoals, editedSecondTeamGoals) => handleSave(2, editedFirstTeamGoals, editedSecondTeamGoals)}
         />
         <hr />
         <ShowResultOfTheGame
           homeClub="Home Club"
           awayClub="Away Club"
-          firstTeamGoals={1}
-          secondTeamGoals={1}
-          onSave={(firstTeamGoals: number, secondTeamGoals: number) => console.log('${firstTeamGoals}-${secondTeamGoals}')}
-        />
-        <hr />
-        <ShowResultOfTheGame
-          homeClub="Home Club"
-          awayClub="Away Club"
-          firstTeamGoals={1}
-          secondTeamGoals={1}
-          onSave={(firstTeamGoals: number, secondTeamGoals: number) => console.log('${firstTeamGoals}-${secondTeamGoals}')}
-        />
-        <hr />
-        <ShowResultOfTheGame
-          homeClub="Home Club"
-          awayClub="Away Club"
-          firstTeamGoals={1}
-          secondTeamGoals={1}
-          onSave={(firstTeamGoals: number, secondTeamGoals: number) => console.log('${firstTeamGoals}-${secondTeamGoals}')}
-        />
-        <hr />
-        <ShowResultOfTheGame
-          homeClub="Home Club"
-          awayClub="Away Club"
-          firstTeamGoals={1}
-          secondTeamGoals={1}
-          onSave={(firstTeamGoals: number, secondTeamGoals: number) => console.log('${firstTeamGoals}-${secondTeamGoals}')}
+          firstTeamGoals={firstTeamGoals[3]}
+          secondTeamGoals={secondTeamGoals[3]}
+          onSave={(editedFirstTeamGoals, editedSecondTeamGoals) => handleSave(3, editedFirstTeamGoals, editedSecondTeamGoals)}
         />
         <hr />
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default Results
+export default Results;
