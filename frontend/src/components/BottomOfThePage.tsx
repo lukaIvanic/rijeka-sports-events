5c8989095b7611f304936cbde4d9c6f01ad04117
cvele3 <cvetkojakov@gmail.com>
2023-01-30 09:00:46 +0100

    changed bottom of the page

--- frontend/src/components/BottomOfThePage.tsx
+++ frontend/src/components/BottomOfThePage.tsx
@@ -3,21 +3,16 @@ import React from 'react'
 export const BottomOfThePage = () => {
 
   return (
-    <footer className="bg-light py-3 fixed-bottom">
-    <div className="container">
-        <div className="row">
-            <div className="col-12 col-md-4">
-                <a href="#" className="text-secondary">Contact Us</a>
+    <footer className="bg-light py-3">
+        <div className="container">
+            <div className="row">
+            <div className="col-12 col-md-4 text-md-center mx-auto">
+                <a href="https://rwt.riteh.hr/home" className="text-secondary">Web Tim</a>
             </div>
-            <div className="col-12 col-md-4 text-md-center">
-                <a href="#" className="text-secondary">Web Tim</a>
-            </div>
-            <div className="col-12 col-md-4 text-md-right">
-                <a href="#" className="text-secondary">About Us</a>
             </div>
         </div>
-    </div>
     </footer>
+
   )
 }
 
