4e16bcfcf8905b37e8271b88b3774cce2cb85e73
cvele3 <cvetkojakov@gmail.com>
2023-01-31 14:45:03 +0100

    added Dashboard and nested routes

--- frontend/src/components/ShowResultOfTheGame.tsx
+++ frontend/src/components/ShowResultOfTheGame.tsx
@@ -35,22 +35,22 @@ const ShowResultOfTheGame: React.FC<Props> = ({ homeClub, awayClub, firstTeamGoa
       <Button color="secondary" onClick={toggle} style={{ marginTop: '10px' }}>Edit</Button>
       <Modal isOpen={modal} toggle={toggle}>
         <ModalHeader toggle={toggle}>Edit Result</ModalHeader>
-        <ModalBody>
-          <Form>
-            <FormGroup>
-              <Label for="firstTeamGoals">First Team Goals</Label>
-              <Input type="number" id="firstTeamGoals" value={editedFirstTeamGoals} onChange={(e) => setEditedFirstTeamGoals(parseInt(e.target.value))} />
-            </FormGroup>
-            <FormGroup>
-              <Label for="secondTeamGoals">Second Team Goals</Label>
-              <Input type="number" id="secondTeamGoals" value={editedSecondTeamGoals} onChange={(e) => setEditedSecondTeamGoals(parseInt(e.target.value))} />
-            </FormGroup>
-          </Form>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={handleSave}>Save</Button>{' '}
-          <Button color="secondary" onClick={toggle}>Cancel</Button>
-        </ModalFooter>
+          <ModalBody>
+            <Form>
+              <FormGroup className="d-flex align-items-center">
+                <img src={logoUrl} style={{ width: '50px', height: '50px', objectFit: 'contain' }} />
+                <Label for="firstTeamGoals" className="mr-2 ml-2">{homeClub}</Label>
+                <Input type="number" id="firstTeamGoals" value={editedFirstTeamGoals} onChange={(e) => setEditedFirstTeamGoals(parseInt(e.target.value))} className="w-25 mr-2" />
+                <Input type="number" id="secondTeamGoals" value={editedSecondTeamGoals} onChange={(e) => setEditedSecondTeamGoals(parseInt(e.target.value))} className="w-25 mr-2" />
+                <Label for="secondTeamGoals" className="">{awayClub}</Label>
+                <img src={logoUrl} style={{ width: '50px', height: '50px', objectFit: 'contain' }} />
+              </FormGroup>
+            </Form>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={handleSave}>Save</Button>
+            <Button color="secondary" onClick={toggle}>Cancel</Button>
+          </ModalFooter>
       </Modal>
     </div>
   )
